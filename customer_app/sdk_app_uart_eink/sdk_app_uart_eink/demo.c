c4db519807d266750d1494b43e7a10b90735862a
lupyuen <luppy@appkaki.com>
2021-02-17 16:32:11 +0800

    Add start transfer handshake

--- customer_app/sdk_app_uart_eink/sdk_app_uart_eink/demo.c
+++ customer_app/sdk_app_uart_eink/sdk_app_uart_eink/demo.c
@@ -27,67 +27,77 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+//  Based on https://github.com/Seeed-Studio/Grove_Triple_Color_E-lnk_2.13/blob/master/examples/Eink_factory_code_213/Eink_factory_code_213.ino
 #include <stdio.h>
+#include <assert.h>
 #include <FreeRTOS.h>
 #include <task.h>
 #include <aos/kernel.h>
 #include <aos/yloop.h>
 #include <vfs.h>
 #include <utils_log.h>
+#include <bl_uart.h>
 #include "demo.h"
 
-static const char *ci_table_step_init[] = CI_CASE_TABLE_STEP1;
-static const char *ci_table_step_send[] = CI_CASE_TABLE_STEP2;
-static const char *ci_table_step_recv[] = CI_CASE_TABLE_STEP3;
-static const char *ci_table_step_end[] = CI_CASE_TABLE_STEP4;
+//  Use UART Port 1 (UART Port 0 is reserved for console)
+#define UART_PORT 1
 
-void log_step(const char *step[2])
+//  Do the Start Transfer Handshake with E-Ink Display:
+//  Receive 'c', send 'a', receive 'b'
+void send_begin() 
 {
-    printf("%s   %s\r\n", step[0], step[1]);
+    //  Wait until 'c' is received
+    for (;;) {
+        //  Read one byte from UART Port, returns -1 if nothing read
+        int ch = bl_uart_data_recv(UART_PORT);
+        if (ch > 0) { printf("%c", ch); }
+        if (ch == 'c') { break; }
+    }
+
+    //  Send 'a'
+    int rc = bl_uart_data_send(UART_PORT, 'a');
+    assert(rc == 0);
+
+    //  Wait until 'b' is received
+    for (;;) {
+        //  Read one byte from UART Port, returns -1 if nothing read
+        int ch = bl_uart_data_recv(UART_PORT);
+        if (ch > 0) { printf("%c", ch); }
+        if (ch == 'b') { break; }
+    }
 }
 
 static void uart_task(void *arg)
 {
-    int length = 0, total = 0;
-    uint8_t buf_recv[128];
-    uint8_t *pbuf = buf_recv;
-    const char *name = arg;
-    const char *send_recv_log = "1234567890abcdefg";
+    //  Init UART Port 1 with Tx Pin 4, Rx Pin 3 for Rx at 230.4 kbps
+    int rc = bl_uart_init(
+        UART_PORT,  //  UART Port 1
+        4,          //  Tx Pin
+        3,          //  Rx Pin
+        255,        //  CTS Unused
+        255,        //  UTS Unused
+        230400      //  Buad Rate
+    );
+    assert(rc == 0);
 
-    int fd = aos_open(name, 0);
-
-    log_info("%s-> fd = %d\r\n", name, fd);
-    if (fd < 0) {
-        return;
-    }
-    memset(buf_recv, 0, sizeof(buf_recv));
-    log_step(ci_table_step_init);
-
-    aos_write(fd, send_recv_log, strlen(send_recv_log));
-    log_step(ci_table_step_send);
-
-    vTaskDelay(1000);
-    length = 0;
-
-    while (1) {
-        length = aos_read(fd, pbuf + total, strlen(send_recv_log));
-        total += length;    
-        
-        if (total != strlen(send_recv_log)) {
-            continue;
-        }
-        if (memcmp(buf_recv, send_recv_log, strlen(send_recv_log)) == 0) {
-            printf("recvbuff:%s\r\n", send_recv_log);
-            log_step(ci_table_step_recv);
-            break;
-        }
-        vTaskDelay(10);
-    }
-    aos_close(fd);
-    log_step(ci_table_step_end);
+    //  Do the Start Transfer Handshake with E-Ink Display
+    send_begin();
 }
 
 void ci_loop_proc()
 {
-    aos_task_new("uart_task", uart_task, "/dev/ttyS1", 2048);
+    aos_task_new("uart_task", uart_task, "", 0);
+}
+
+/// TODO: We now show assertion failures in development.
+/// For production, comment out this function to use the system default,
+/// which loops forever without messages.
+void __assert_func(const char *file, int line, const char *func, const char *failedexpr)
+{
+    //  Show the assertion failure, file, line, function name
+	printf("Assertion Failed \"%s\": file \"%s\", line %d%s%s\r\n",
+        failedexpr, file, line, func ? ", function: " : "",
+        func ? func : "");
+	//  Loop forever, do not pass go, do not collect $200
+	for (;;) {}
 }
