6eecc5f6bd8d57170955f0ea3bebbdf6203e3a33
lupyuen <luppy@appkaki.com>
2021-03-22 10:35:02 +0800

    Test GPIO handler

--- customer_app/sdk_app_lora/sdk_app_lora/sx1276-board.c
+++ customer_app/sdk_app_lora/sdk_app_lora/sx1276-board.c
@@ -455,13 +455,7 @@ static int enqueue_interrupt_event(
     }
 #endif  //  TODO
 
-    //  After 1 interrupt, we suppress interrupts to troubleshoot the 
-    //  hanging upon receiving a LoRa Packet.
-    //  TODO: Always enable interrupts
-    if (g_dio0_counter + g_dio1_counter + g_dio2_counter + g_dio3_counter
-        + g_dio4_counter + g_dio5_counter + g_nodio_counter < 1) {
-        //  Enable GPIO Interrupt for the pin
-        //  TODO: bl_gpio_intmask(gpioPin, 0);
-    }
+    //  Enable GPIO Interrupt for the pin
+    //  TODO: bl_gpio_intmask(gpioPin, 0);
     return 0;
 }
