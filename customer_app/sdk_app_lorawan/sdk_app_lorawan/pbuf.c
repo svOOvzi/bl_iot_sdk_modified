7f7592279ae6f206bb56e347f138edd25aafdeaf
lupyuen <luppy@appkaki.com>
2021-04-09 21:55:17 +0800

    Clean up

--- customer_app/sdk_app_lorawan/sdk_app_lorawan/pbuf.c
+++ customer_app/sdk_app_lorawan/sdk_app_lorawan/pbuf.c
@@ -13,84 +13,84 @@
 /// Then dump the header and payload.
 void test_pbuf(char *buf0, int len0, int argc, char **argv)
 {
-	//  Header bytes to be set (3 bytes)
-	uint8_t header[3] = { 0x11, 0x22, 0x33 };
+    //  Header bytes to be set (3 bytes)
+    uint8_t header[3] = { 0x11, 0x22, 0x33 };
 
-	//  Payload bytes to be set (5 bytes)
-	uint8_t payload[5] = { 0x01, 0x02, 0x03, 0x04, 0x05 };
+    //  Payload bytes to be set (5 bytes)
+    uint8_t payload[5] = { 0x01, 0x02, 0x03, 0x04, 0x05 };
 
-	//  Init LWIP Buffer Pool
-	lwip_init();
+    //  Init LWIP Buffer Pool
+    lwip_init();
 
-	//  Allocate a pbuf Packet Buffer
-	struct pbuf *buf = pbuf_alloc(
-		PBUF_TRANSPORT,   //  Buffer will include UDP Transport Header
-		sizeof(payload),  //  Payload size
-		PBUF_RAM          //  Allocate a single block of RAM
-	);                    //  TODO: Switch to pooled memory (PBUF_POOL), which is more efficient
-	assert(buf != NULL);
+    //  Allocate a pbuf Packet Buffer
+    struct pbuf *buf = pbuf_alloc(
+        PBUF_TRANSPORT,   //  Buffer will include UDP Transport Header
+        sizeof(payload),  //  Payload size
+        PBUF_RAM          //  Allocate a single block of RAM
+    );                    //  TODO: Switch to pooled memory (PBUF_POOL), which is more efficient
+    assert(buf != NULL);
 
-	//  Payload pointer now points to the payload.
-	printf("Before Shift: Packet buffer addr=%p, payload=%p, len=%d, tot_len=%d\r\n", buf, buf->payload, buf->len, buf->tot_len);
+    //  Payload pointer now points to the payload.
+    printf("Before Shift: Packet buffer addr=%p, payload=%p, len=%d, tot_len=%d\r\n", buf, buf->payload, buf->len, buf->tot_len);
 
-	//  Shift the pbuf payload pointer BACKWARD 
-	//  to accommodate the header (3 bytes).
-	u8_t rc = pbuf_add_header(buf, sizeof(header));
-	assert(rc == 0);
-	printf("After Shift:  Packet buffer addr=%p, payload=%p, len=%d, tot_len=%d\r\n", buf, buf->payload, buf->len, buf->tot_len);
+    //  Shift the pbuf payload pointer BACKWARD 
+    //  to accommodate the header (3 bytes).
+    u8_t rc = pbuf_add_header(buf, sizeof(header));
+    assert(rc == 0);
+    printf("After Shift:  Packet buffer addr=%p, payload=%p, len=%d, tot_len=%d\r\n", buf, buf->payload, buf->len, buf->tot_len);
 
-	//  Payload pointer now points to the header.
-	//  We set the header in the pbuf.
-	printf("Set header\r\n");
-	memcpy(buf->payload, header, sizeof(header));
+    //  Payload pointer now points to the header.
+    //  We set the header in the pbuf.
+    printf("Set header\r\n");
+    memcpy(buf->payload, header, sizeof(header));
 
-	//  Shift the pbuf payload pointer FORWARD 
-	//  to skip the header (3 bytes).
-	rc = pbuf_remove_header(buf, sizeof(header));
-	assert(rc == 0);
-	printf("Undo Shift:   Packet buffer addr=%p, payload=%p, len=%d, tot_len=%d\r\n", buf, buf->payload, buf->len, buf->tot_len);
+    //  Shift the pbuf payload pointer FORWARD 
+    //  to skip the header (3 bytes).
+    rc = pbuf_remove_header(buf, sizeof(header));
+    assert(rc == 0);
+    printf("Undo Shift:   Packet buffer addr=%p, payload=%p, len=%d, tot_len=%d\r\n", buf, buf->payload, buf->len, buf->tot_len);
 
-	//  Payload pointer now points to the payload.
-	//  We set the payload in the pbuf.
-	printf("Set payload\r\n");
-	memcpy(buf->payload, payload, sizeof(payload));
+    //  Payload pointer now points to the payload.
+    //  We set the payload in the pbuf.
+    printf("Set payload\r\n");
+    memcpy(buf->payload, payload, sizeof(payload));
 
-	//  Now we dump the header and payload
-	//  by shifting the payload pointer.
-	printf("Dumping header and payload...\r\n");
+    //  Now we dump the header and payload
+    //  by shifting the payload pointer.
+    printf("Dumping header and payload...\r\n");
 
-	//  Shift the pbuf payload pointer BACKWARD (3 bytes)
-	//  to locate the header.
-	rc = pbuf_add_header(buf, sizeof(header));
-	assert(rc == 0);
+    //  Shift the pbuf payload pointer BACKWARD (3 bytes)
+    //  to locate the header.
+    rc = pbuf_add_header(buf, sizeof(header));
+    assert(rc == 0);
 
-	//  Dump the header
+    //  Dump the header
     printf("Packet buffer header: \r\n");
-	uint8_t *p = buf->payload;   //  Get the header
-	int len = sizeof(header);  //  Header is always fixed size
+    uint8_t *p = buf->payload;   //  Get the header
+    int len = sizeof(header);  //  Header is always fixed size
     for (int i = 0; i < len; i++) {
         printf("%02x ", p[i]);
     }
     printf("\r\n");
-	//  Should show the header: 0x11, 0x22, 0x33
+    //  Should show the header: 0x11, 0x22, 0x33
 
-	//  Shift the pbuf payload pointer FORWARD (3 bytes)
-	//  to locate the payload.
-	rc = pbuf_remove_header(buf, sizeof(header));
-	assert(rc == 0);
+    //  Shift the pbuf payload pointer FORWARD (3 bytes)
+    //  to locate the payload.
+    rc = pbuf_remove_header(buf, sizeof(header));
+    assert(rc == 0);
 
-	//  Dump the payload
+    //  Dump the payload
     printf("Packet buffer payload: \r\n");
-	p = buf->payload;  //  Get the payload
-	len = buf->len;    //  Payload size is variable
+    p = buf->payload;  //  Get the payload
+    len = buf->len;    //  Payload size is variable
     for (int i = 0; i < len; i++) {
         printf("%02x ", p[i]);
     }
     printf("\r\n");
-	//  Should show the payload: 0x01, 0x02, 0x03, 0x04, 0x05
+    //  Should show the payload: 0x01, 0x02, 0x03, 0x04, 0x05
 
-	//  Free the pbuf
-	pbuf_free(buf);
+    //  Free the pbuf
+    pbuf_free(buf);
 }
 
 #ifdef NOTUSED
