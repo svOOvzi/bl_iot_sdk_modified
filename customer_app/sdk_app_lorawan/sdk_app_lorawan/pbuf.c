e20c163b359461989cac8bf6af59b679e4596cbe
lupyuen <luppy@appkaki.com>
2021-04-09 21:47:24 +0800

    pbuf header and payload OK

--- customer_app/sdk_app_lorawan/sdk_app_lorawan/pbuf.c
+++ customer_app/sdk_app_lorawan/sdk_app_lorawan/pbuf.c
@@ -8,16 +8,21 @@
 #include "lwip/pbuf.h"  //  For Lightweight IP Stack pbuf 
 #include "demo.h"
 
-/// Test pbuf Packet Buffer from LWIP
+/// Test pbuf Packet Buffer from Lightweight IP Stack.
+/// We allocate a Packet Buffer, set the header and payload.
+/// Then dump the header and payload.
 void test_pbuf(char *buf0, int len0, int argc, char **argv)
 {
-	//  Payload to be set
-	uint8_t payload[3] = { 0x01, 0x02, 0x03 };
+	//  Header bytes to be set (3 bytes)
+	uint8_t header[3] = { 0x11, 0x22, 0x33 };
+
+	//  Payload bytes to be set (5 bytes)
+	uint8_t payload[5] = { 0x01, 0x02, 0x03, 0x04, 0x05 };
 
 	//  Init LWIP Buffer Pool
 	lwip_init();
 
-	//  Create a pbuf Packet Buffer
+	//  Allocate a pbuf Packet Buffer
 	struct pbuf *buf = pbuf_alloc(
 		PBUF_TRANSPORT,   //  Buffer will include UDP Transport Header
 		sizeof(payload),  //  Payload size
@@ -25,81 +30,83 @@ void test_pbuf(char *buf0, int len0, int argc, char **argv)
 	);                    //  TODO: Switch to pooled memory (PBUF_POOL), which is more efficient
 	assert(buf != NULL);
 
-	//  Set header in the pbuf
-	uint8_t header[3] = { 0x11, 0x22, 0x33 };
+	//  Payload pointer now points to the payload.
+	printf("Before Shift: Packet buffer addr=%p, payload=%p, len=%d, tot_len=%d\r\n", buf, buf->payload, buf->len, buf->tot_len);
 
-	//  Set payload in the pbuf
+	//  Shift the pbuf payload pointer BACKWARD 
+	//  to accommodate the header (3 bytes).
+	u8_t rc = pbuf_add_header(buf, sizeof(header));
+	assert(rc == 0);
+	printf("After Shift:  Packet buffer addr=%p, payload=%p, len=%d, tot_len=%d\r\n", buf, buf->payload, buf->len, buf->tot_len);
+
+	//  Payload pointer now points to the header.
+	//  We set the header in the pbuf.
+	printf("Set header\r\n");
+	memcpy(buf->payload, header, sizeof(header));
+
+	//  Shift the pbuf payload pointer FORWARD 
+	//  to skip the header (3 bytes).
+	rc = pbuf_remove_header(buf, sizeof(header));
+	assert(rc == 0);
+	printf("Undo Shift:   Packet buffer addr=%p, payload=%p, len=%d, tot_len=%d\r\n", buf, buf->payload, buf->len, buf->tot_len);
+
+	//  Payload pointer now points to the payload.
+	//  We set the payload in the pbuf.
+	printf("Set payload\r\n");
 	memcpy(buf->payload, payload, sizeof(payload));
 
-	//  Dump the pbuf
-	printf("Before Header: Packet buffer addr=%p, payload=%p, len=%d, tot_len=%d\r\n", buf, buf->payload, buf->len, buf->tot_len);
+	//  Now we dump the header and payload
+	//  by shifting the payload pointer.
+	printf("Dumping header and payload...\r\n");
 
-	//  Make room for the header. payload will shift back 3 bytes, len and tot_len will increase by 3.
-	u8_t rc = pbuf_header(buf, sizeof(header));
+	//  Shift the pbuf payload pointer BACKWARD (3 bytes)
+	//  to locate the header.
+	rc = pbuf_add_header(buf, sizeof(header));
 	assert(rc == 0);
 
-	//  Dump the pbuf
-	printf("After Header: Packet buffer addr=%p, payload=%p, len=%d, tot_len=%d\r\n", buf, buf->payload, buf->len, buf->tot_len);
-
 	//  Dump the header
-    printf("Packet buffer header\r\n");
-	int header_len = ((void *) buf->payload) - ((void *) buf);
-	uint8_t *p = (uint8_t *) buf;
-    for (int i = 0; i < header_len; i++) {
+    printf("Packet buffer header: \r\n");
+	uint8_t *p = buf->payload;   //  Get the header
+	int len = sizeof(header);  //  Header is always fixed size
+    for (int i = 0; i < len; i++) {
         printf("%02x ", p[i]);
     }
     printf("\r\n");
+	//  Should show the header: 0x11, 0x22, 0x33
+
+	//  Shift the pbuf payload pointer FORWARD (3 bytes)
+	//  to locate the payload.
+	rc = pbuf_remove_header(buf, sizeof(header));
+	assert(rc == 0);
 
 	//  Dump the payload
     printf("Packet buffer payload: \r\n");
-	p = buf->payload;
-    for (int i = 0; i < buf->len; i++) {
+	p = buf->payload;  //  Get the payload
+	len = buf->len;    //  Payload size is variable
+    for (int i = 0; i < len; i++) {
         printf("%02x ", p[i]);
     }
     printf("\r\n");
+	//  Should show the payload: 0x01, 0x02, 0x03, 0x04, 0x05
 
 	//  Free the pbuf
 	pbuf_free(buf);
 }
 
-/*
-	lwip_init();
-	struct udp_pcb * pUdpConnection = udp_new();
-	char data[3] = { 0x00, 0x00, 0x00 };
-	struct pbuf* b = pbuf_alloc(PBUF_TRANSPORT, 3, PBUF_RAM);
-	os_memcpy (b->payload, data, 3);
-	udp_sendto(pcb, b, IP_ADDR_BROADCAST, 9090);
-	pbuf_free(b);
-*/
+#ifdef NOTUSED
+Output Log:
 
-/*
-	#include "lwipopts.h"
-	#include "lwip/sockets.h"
-	#include "lwip/ip_addr.h"
-	#include "lwip/init.h"
-	#include "lwip/netif.h"
-	#include "lwip/igmp.h"
-	#include "lwip/udp.h"	
+# test_pbuf
+-------------------->>>>>>>> LWIP tcp_port 57705
+Before Shift: Packet buffer addr=0x42030008, payload=0x420300d0, len=5, tot_len=5
+After Shift:  Packet buffer addr=0x42030008, payload=0x420300cd, len=8, tot_len=8
+Set header
+Undo Shift:   Packet buffer addr=0x42030008, payload=0x420300d0, len=5, tot_len=5
+Set payload
+Dumping header and payload...
+Packet buffer header: 
+11 22 33 
+Packet buffer payload: 
+01 02 03 04 05 
 
-	void ICACHE_FLASH_ATTR handle_udp_recv(void *arg, struct udp_pcb *pcb, struct pbuf *p,  ip_addr_t *addr, u16_t port) {
-		int length = p->len;
-		char * pusrdata = p->payload;
-		os_printf("Received udp data: %s \r\n", pusrdata);
-		pbuf_free(p);
-	}
-
-	void init_udp() {
-		struct ip_addr ipSend;
-		lwip_init();
-		struct udp_pcb * pUdpConnection = udp_new();
-		IP4_ADDR(&ipSend, 255, 255, 255, 255);
-		pUdpConnection->multicast_ip = ipSend;
-		pUdpConnection->remote_ip = ipSend;
-		pUdpConnection->remote_port = 8080;
-		if(pUdpConnection == NULL) {
-			os_printf("\nCould not create new udp socket... \n");
-		}
-		err = udp_bind(pUdpConnection, IP_ADDR_ANY, 8080);
-		udp_recv(pUdpConnection, handle_udp_recv, pUdpConnection);
-	}
-*/
+#endif  //  NOTUSED
