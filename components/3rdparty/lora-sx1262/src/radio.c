516bf81cefdcd1dc10de2d56559ea1087c71916e
lupyuen <luppy@appkaki.com>
2021-04-14 12:27:31 +0800

    Fixing build

--- components/3rdparty/lora-sx1262/src/radio.c
+++ components/3rdparty/lora-sx1262/src/radio.c
@@ -31,6 +31,153 @@
 //  Timer definition for BL602
 typedef struct ble_npl_callout TimerEvent_t;
 
+//  BL602 Functions for LoRa
+#include <device/vfs_spi.h>  //  For spi_ioc_transfer_t
+#include <hal/soc/spi.h>     //  For hal_spi_transfer
+#include <hal_spi.h>         //  For spi_init
+#include <bl_gpio.h>         //  For bl_gpio_output_set
+#include <bl602_glb.h>       //  For GLB_GPIO_Func_Init
+#include "nimble_npl.h"      //  For NimBLE Porting Layer (timer functions)
+
+///////////////////////////////////////////////////////////////////////////////
+//  Timer Functions
+
+/// Initialise a timer
+void TimerInit(
+    struct ble_npl_callout *timer,  //  The timer to initialize. Cannot be NULL.
+    ble_npl_event_fn *f)            //  The timer callback function. Cannot be NULL.
+{
+    //  Implement with Callout Functions from NimBLE Porting Layer
+    assert(timer != NULL);
+    assert(f != NULL);
+
+    //  Event Queue containing Events to be processed, defined in demo.c.  TODO: Move to header file.
+    extern struct ble_npl_eventq event_queue;
+
+    //  Init the Callout Timer with the Callback Function
+    ble_npl_callout_init(
+        timer,         //  Callout Timer
+        &event_queue,  //  Event Queue that will handle the Callout upon timeout
+        f,             //  Callback Function
+        NULL           //  Argument to be passed to Callback Function
+    );
+}
+
+/// Stops a timer from running.  Can be called even if timer is not running.
+/// Based on https://mynewt.apache.org/latest/os/core_os/cputime/os_cputime.html#c.os_cputime_timer_stop
+void os_cputime_timer_stop(
+    struct ble_npl_callout *timer)  //  Pointer to timer to stop. Cannot be NULL.
+{
+    //  Implement with Callout Functions from NimBLE Porting Layer
+    assert(timer != NULL);
+
+    //  If Callout Timer is still running...
+    if (ble_npl_callout_is_active(timer)) {
+        //  Stop the Callout Timer
+        ble_npl_callout_stop(timer);
+    }
+}
+
+/// Sets a timer that will expire ‘usecs’ microseconds from the current time.
+/// NOTE: This must be called when the timer is stopped.
+/// Based on https://mynewt.apache.org/latest/os/core_os/cputime/os_cputime.html#c.os_cputime_timer_relative
+void os_cputime_timer_relative(
+    struct ble_npl_callout *timer,  //  Pointer to timer. Cannot be NULL.
+    uint32_t microsecs)             //  The number of microseconds from now at which the timer will expire.
+{
+    //  Implement with Callout Functions from NimBLE Porting Layer.
+    //  Assume that Callout Timer has been stopped.
+    assert(timer != NULL);
+
+    //  Convert microseconds to ticks
+    ble_npl_time_t ticks = ble_npl_time_ms_to_ticks32(
+        microsecs / 1000  //  Duration in milliseconds
+    );
+
+    //  Wait at least 1 tick
+    if (ticks == 0) { ticks = 1; }
+
+    //  Trigger the Callout Timer after the elapsed ticks
+    ble_npl_error_t rc = ble_npl_callout_reset(
+        timer,  //  Callout Timer
+        ticks   //  Number of ticks
+    );
+    assert(rc == 0);
+}
+
+/// Wait until ‘millisecs’ milliseconds has elapsed. This is a blocking delay.
+void DelayMs(uint32_t millisecs)  //  The number of milliseconds to wait.
+{
+    //  Implement with Timer Functions from NimBLE Porting Layer.
+    //  Convert milliseconds to ticks.
+    ble_npl_time_t ticks = ble_npl_time_ms_to_ticks32(
+        millisecs  //  Duration in milliseconds
+    );
+
+    //  Wait at least 1 tick
+    if (ticks == 0) { ticks = 1; }
+
+    //  Wait for the ticks
+    ble_npl_time_delay(ticks);
+}
+
+/// Return current time in microseconds
+uint32_t TimerGetCurrentTime(void)
+{
+    //  Convert ticks to milliseconds then microseconds
+    return xTaskGetTickCount() * portTICK_PERIOD_MS * 1000;
+}
+
+/// Return elased time in microseconds
+uint32_t TimerGetElapsedTime(uint32_t saved_time)
+{
+    return TimerGetCurrentTime() - saved_time;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//  SPI Functions
+
+/// SPI Device Instance. TODO: Move to sx1276.h
+extern spi_dev_t spi_device;
+
+/// SPI Transmit Buffer (1 byte)
+static uint8_t spi_tx_buf[1];
+
+/// SPI Receive Buffer (1 byte)
+static uint8_t spi_rx_buf[1];
+
+/// Blocking call to send a value on the SPI. Returns the value received from the SPI Peripheral.
+/// Assume that we are sending and receiving 8-bit values on SPI.
+/// Assume Chip Select Pin has already been set to Low by caller.
+/// TODO: We should combine multiple SPI DMA Requests, instead of handling one byte at a time
+uint16_t hal_spi_tx_val(int spi_num, uint16_t val) {
+    //  Populate the transmit buffer
+    spi_tx_buf[0] = val;
+
+    //  Clear the receive buffer
+    memset(&spi_rx_buf, 0, sizeof(spi_rx_buf));
+
+    //  Prepare SPI Transfer
+    static spi_ioc_transfer_t transfer;
+    memset(&transfer, 0, sizeof(transfer));    
+    transfer.tx_buf = (uint32_t) spi_tx_buf;  //  Transmit Buffer
+    transfer.rx_buf = (uint32_t) spi_rx_buf;  //  Receive Buffer
+    transfer.len    = 1;                      //  How many bytes
+
+    //  Assume Chip Select Pin has already been set to Low by caller.
+
+    //  Execute the SPI Transfer with the DMA Controller
+    int rc = hal_spi_transfer(
+        &spi_device,  //  SPI Device
+        &transfer,    //  SPI Transfers
+        1             //  How many transfers (Number of requests, not bytes)
+    );
+    assert(rc == 0);
+
+    //  Return the received byte
+    return spi_rx_buf[0];
+}
+
 /*!
  * \brief Initializes the radio
  *
@@ -449,12 +596,12 @@ void RadioOnDioIrq( void* context );
 /*!
  * \brief Tx timeout timer callback
  */
-void RadioOnTxTimeoutIrq( void* context );
+void RadioOnTxTimeoutIrq( struct ble_npl_event *ev );
 
 /*!
  * \brief Rx timeout timer callback
  */
-void RadioOnRxTimeoutIrq( void* context );
+void RadioOnRxTimeoutIrq( struct ble_npl_event *ev );
 
 /*
  * Private global variables
@@ -1233,16 +1380,18 @@ uint32_t RadioGetWakeupTime( void )
     return SX126xGetBoardTcxoWakeupTime( ) + RADIO_WAKEUP_TIME;
 }
 
-void RadioOnTxTimeoutIrq( void* context )
+void RadioOnTxTimeoutIrq( struct ble_npl_event *ev )
 {
+    printf("RadioOnTxTimeoutIrq\r\n");
     if( ( RadioEvents.TxTimeout != NULL ) )
     {
         RadioEvents.TxTimeout( );
     }
 }
 
-void RadioOnRxTimeoutIrq( void* context )
+void RadioOnRxTimeoutIrq( struct ble_npl_event *ev )
 {
+    printf("RadioOnRxTimeoutIrq\r\n");
     if( ( RadioEvents.RxTimeout != NULL ) )
     {
         RadioEvents.RxTimeout( );
@@ -1251,11 +1400,13 @@ void RadioOnRxTimeoutIrq( void* context )
 
 void RadioOnDioIrq( void* context )
 {
+    printf("RadioOnDioIrq\r\n");
     IrqFired = true;
 }
 
 void RadioIrqProcess( void )
 {
+    printf("RadioIrqProcess\r\n");
     CRITICAL_SECTION_BEGIN( );
     // Clear IRQ flag
     const bool isIrqFired = IrqFired;
