f5a6d90a406ab59d0b286140bafce816d521a3b4
lupyuen <luppy@appkaki.com>
2021-04-18 11:25:13 +0800

    Fix NimBLE

--- customer_app/sdk_app_rust/sdk_app_rust/nimble.c
+++ customer_app/sdk_app_rust/sdk_app_rust/nimble.c
@@ -1,7 +1,16 @@
 //  Export the inline functions for NimBLE Porting Layer to Rust
+//  TODO: Move this to nimble-porting-layer library
 
-//  Disable inline
+//  Include FreeRTOS before NPL, so that FreeRTOS will be inlined
+#include "FreeRTOS.h"
+
+//  Disable static inline so:
+//    static inline void ble_npl_time_delay(ble_npl_time_t ticks) { ... }
+//  Becomes:
+//    void ble_npl_time_delay(ble_npl_time_t ticks) { ... }
+#define static
 #define inline
 
-//  Define the functions
+//  Define the functions like:
+//    void ble_npl_time_delay(ble_npl_time_t ticks) { ... }
 #include "nimble_npl.h"
