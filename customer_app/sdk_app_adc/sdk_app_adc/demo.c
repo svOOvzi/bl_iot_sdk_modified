9d4012ded45ff14e79c021345b7d2b3813734c63
lupyuen <luppy@appkaki.com>
2021-07-22 09:00:18 +0800

    Read 32 samples of GPIO 11

--- customer_app/sdk_app_adc/sdk_app_adc/demo.c
+++ customer_app/sdk_app_adc/sdk_app_adc/demo.c
@@ -16,14 +16,13 @@
 void init_adc(char *buf, int len, int argc, char **argv) {
     //  Only these GPIOs are supported: 4, 5, 6, 9, 10, 11, 12, 13, 14, 15
     assert(ADC_GPIO==4 || ADC_GPIO==5 || ADC_GPIO==6 || ADC_GPIO==9 || ADC_GPIO==10 || ADC_GPIO==11 || ADC_GPIO==12 || ADC_GPIO==13 || ADC_GPIO==14 || ADC_GPIO==15);
-    
+
     //  We set the ADC Frequency to 10 kHz according to https://wiki.analog.com/university/courses/electronics/electronics-lab-led-sensor?rev=1551786227
     //  This is 10,000 samples per second.
-    //  We shall read 1000 samples, which will take 0.1 seconds.
     int rc = hal_adc_init(
         1,  //  Single-Channel Conversion Mode
         10000,  //  Frequency
-        1000,  //  Number of Samples
+        32,  //  Number of Samples
         ADC_GPIO  //  GPIO Pin Number
     );
     assert(rc == 0);
@@ -34,7 +33,7 @@ void read_adc(char *buf, int len, int argc, char **argv) {
     //  Read the ADC Channel via DMA. Returns -1 in case of error.
     int val = hal_adc_get_data(
         ADC_GPIO,  //  GPIO Pin Number
-        0  //  Raw Flag
+        1  //  Raw Flag
     );
     //  Raw Flag = 0: Returns raw value between 0 to 65535
     //  Raw Flag = 1: Returns scaled value between 0 to 3199
