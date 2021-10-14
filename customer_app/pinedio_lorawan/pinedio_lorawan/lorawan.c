bde939dff8b8468e0d407a27f84b292875327322
Lee Lup Yuen <luppy@appkaki.com>
2021-10-14 10:16:40 +0800

    Fix offset

--- customer_app/pinedio_lorawan/pinedio_lorawan/lorawan.c
+++ customer_app/pinedio_lorawan/pinedio_lorawan/lorawan.c
@@ -1311,7 +1311,9 @@ static int get_tsen_adc(
     static uint16_t tsen_offset = 0xFFFF;
     float val = 0.0;
 
+    //  If the offset has not been fetched...
     if (0xFFFF == tsen_offset) {
+        //  Define the ADC configuration
         tsen_offset = 0;
         ADC_CFG_Type adcCfg = {
             .v18Sel=ADC_V18_SEL_1P82V,                /*!< ADC 1.8V select */
@@ -1328,43 +1330,37 @@ static int get_tsen_adc(
             .offsetCalibEn=0,                         /*!< Offset calibration enable */
             .offsetCalibVal=0,                        /*!< Offset calibration value */
         };
-
-
         ADC_FIFO_Cfg_Type adcFifoCfg = {
             .fifoThreshold = ADC_FIFO_THRESHOLD_1,
             .dmaEn = DISABLE,
         };
 
+        //  Enable and reset the ADC
         GLB_Set_ADC_CLK(ENABLE,GLB_ADC_CLK_96M, 7);
-
         ADC_Disable();
         ADC_Enable();
-
         ADC_Reset();
 
+        //  Configure the ADC and Internal Temperature Sensor
         ADC_Init(&adcCfg);
         ADC_Channel_Config(ADC_CHAN_TSEN_P, ADC_CHAN_GND, 0);
         ADC_Tsen_Init(ADC_TSEN_MOD_INTERNAL_DIODE);
-
         ADC_FIFO_Cfg(&adcFifoCfg);
 
-        if (ADC_Trim_TSEN(&tsen_offset) == ERROR) {
-            printf("read efuse data failed\r\n");
-        }
-        assert(ADC_Trim_TSEN(&tsen_offset) != ERROR);
+        //  Fetch the offset
+        BL_Err_Type rc = ADC_Trim_TSEN(&tsen_offset);
+        assert(rc != ERROR);  //  Read efuse data failed
 
         //  Must wait 100 milliseconds or returned temperature will be negative
         vTaskDelay(100 / portTICK_PERIOD_MS);
     }
+    //  Read the temperature based on the offset
     val = TSEN_Get_Temp(tsen_offset);
     if (log_flag) {
         printf("offset = %d\r\n", tsen_offset);
         printf("temperature = %f Celsius\r\n", val);
     }
-
-    if (temp) {
-        *temp = val;
-    }
-
+    //  Return the temperature
+    *temp = val;
     return 0;
 }
